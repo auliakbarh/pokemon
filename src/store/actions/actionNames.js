9adfb85a213d854726b7f7a657c2cbbb4c358769
auliakbarh <auliaakbarharahaap@gmail.com>
2020-03-05 05:24:52 +0700

    add action names

--- src/store/actions/actionNames.js
+++ src/store/actions/actionNames.js
@@ -1,3 +1,7 @@
 export const FETCH_LIST_POKEMON = 'fetching list of pokemons';
 export const FETCH_LIST_POKEMON_SUCCESS = 'fetching list of pokemons success';
 export const FETCH_LIST_POKEMON_FAILED = 'fetching list of pokemons failed';
+
+export const FETCH_DETAIL_OF_POKEMON = 'fetching detail of pokemon by id';
+export const FETCH_DETAIL_OF_POKEMON_SUCCESS = 'fetching detail of pokemon by id success';
+export const FETCH_DETAIL_OF_POKEMON_FAILED = 'fetching detail of pokemon by id failed';
