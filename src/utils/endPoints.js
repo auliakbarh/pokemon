2508550a93e2a2ea9ba2e592a3535ca0748f2476
auliakbarh <auliaakbarharahaap@gmail.com>
2020-03-04 22:42:16 +0700

    add endpoints of get detail of pokemon

--- src/utils/endPoints.js
+++ src/utils/endPoints.js
@@ -1,3 +1,4 @@
-const API_URL = 'https://pokeapi.co/api/v2/';
+export const API_URL = 'https://pokeapi.co/api/v2/';
 
 export const GET_LIST_OF_POKEMONS = (offset) => API_URL + `pokemon?offset=${offset}&limit=20`;
+export const GET_DETAIL_OF_POKEMON = (id) => API_URL + `pokemon/${id}`;
